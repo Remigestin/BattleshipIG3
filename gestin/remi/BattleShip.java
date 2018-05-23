99beb3c006560d3091c1c4b74550642e4ca26b30
Gestin <gestin.remi@gmail.com>
2018-05-23 09:50:02 +0200

    correction retry

--- gestin/remi/BattleShip.java
+++ gestin/remi/BattleShip.java
@@ -39,8 +39,8 @@ public class BattleShip {
 				String choice = sc.nextLine();
 
 				if (choice.equals("1")) {
-					Player p1 = new Human();
-					Player p2 = new Human();
+					p1 = new Human();
+					p2 = new Human();
 
 					valid = true;
 
